68e6bd7f8cee4fee7e1c674e974b180f01d719b3
Xenare <steamgaterman@gmail.com>
2026-05-29 12:12:18 +0500

    I am a legend

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import unittest
 from add import *
+from subtract import *
+from test_subtract import *
 
 def main():
-    x = 1
+    x = 2
     y = 1
     print("x+y is", add(x,y))
+    print("x-y is", subtract(x,y))
 
 if __name__ == "__main__":
     main()
+    unittest.main()
