08070d8474604bf5dfb0e8814277df1df0108be6
Farzona <farzona18nanona@gmail.com>
2026-06-02 00:04:18 +0500

    added the part for the multiplication with addition

---
+++ test_multiply_by_add.py
@@ -0,0 +1,12 @@
+import unittest
+# Импортируем add (на будущее) и multiply_by_add (которого еще нет)
+from add import add 
+from multiply_by_add import multiply_by_add
+
+class TestMultiplyByAdd(unittest.TestCase):
+    def test_multiply_positive_numbers(self):
+        # Этот вызов вызовет ошибку, так как функции еще нет
+        self.assertEqual(multiply_by_add(3, 4), 12)
+
+if __name__ == '__main__':
+    unittest.main()
