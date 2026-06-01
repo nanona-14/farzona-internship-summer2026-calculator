319cfa99abb984081069cbfec73da9c8b213c4cd
Farzona <farzona18nanona@gmail.com>
2026-06-01 23:15:46 +0500

    fix: add type validation to multiply function

--- multiply.py
+++ multiply.py
@@ -1,2 +1,6 @@
-def multiply (a, b):
+def multiply(a, b):
+    # Добавляем валидацию типов (разрешаем только целые int и дробные float)
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise TypeError("Оба аргумента должны быть числами (int или float)")
+        
     return a * b
